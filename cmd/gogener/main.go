c38bb013376b2f0b64faca37ef7db5efaeb83bea
ryomak <ryooomaaa0413@outlook.com>
2020-02-23 12:02:49 +0900

    fix version setting

--- cmd/gogener/main.go
+++ cmd/gogener/main.go
@@ -6,7 +6,11 @@ import (
 	"github.com/ryomak/gogener/app"
 )
 
+var (
+  Version = "unset"
+)
+
 func main() {
-	app := app.New("gogener", "CLI tool that automatically creates apps using templates", "1.0.0")
+	app := app.New("gogener", "CLI tool that automatically creates apps using templates", Version)
 	app.Run(os.Args)
 }
